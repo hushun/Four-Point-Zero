3a1b1154515fdf0773efb77c3ed715e01d426a84
Shun Hu <hushun@msu.edu>
2020-01-21 01:28:49 -0500

    lab 1 update

--- Lab 1/Lab 1.R
+++ Lab 1/Lab 1.R
@@ -49,7 +49,7 @@ p + geom_point() +
 # The dollar() changes the labels of x-axis to dollors.
 
 # How can you find other ways of relabeling the scales when using scale_x_log10() ?
-# ？change the value after scales:: , to number for instance.
+# Change the value after scales:: , to number for instance.
 p + geom_point() +
   geom_smooth(method = "lm") +
   scale_x_log10(labels = scales::number)
@@ -65,17 +65,16 @@ p + geom_point() + scale_x_log10()
 # p + geom_point(color="yellow") + scale_x_log10()
 p + geom_point(color="yellow") + scale_x_log10()
 
-
 p <- ggplot(data = gapminder,
             mapping = aes(x = gdpPercap, y = lifeExp))
 p + geom_point() + geom_smooth(color = "orange", se = FALSE, size = 8, method = "lm") + scale_x_log10()
 # Try it: Write down what all those arguments in geom_smooth(...) do
 # geom_smooth(...) generates a linear regression line for the given data and plot the line.
-# se : Display confidence interval around smooth?  color: color of the regression line.  method="lm" : a function of smooth called lm.
+# Display confidence interval around smooth? color: color of the regression line.  method="lm" : a function of smooth called lm.
 
-p + geom_point(alpha = 0.3) +  #tranparency
-  geom_smooth(method = "gam") +   #one of method function
-  scale_x_log10(labels = scales::dollar) +    #change unit to dollar.
+p + geom_point(alpha = 0.3) +
+  geom_smooth(method = "gam") +
+  scale_x_log10(labels = scales::dollar) +
   labs(x = "GDP Per Capita", y = "Life Expectancy in Years",
        title = "Economic Growth and Life Expectancy", subtitle = "Data Points are country-years", caption = "Source: Gapminder")
 
@@ -86,16 +85,14 @@ p + geom_point()
 p + geom_point() + scale_x_log10(labels = dollar)
 p + geom_point() + scale_x_log10(labels = dollar) + geom_smooth()
 # Try it: What does fill = continent do? What do you think about the match of colors between lines and error bands?
-# color the error bands of the regression line. 
-# match of colors between lines and error bands are important, in this way people can easily distinguish different lines and its error bands. 
+# Color the error bands of the regression line. 
+# Match of colors between lines and error bands are important, in this way people can easily distinguish different lines and its error bands. 
 
 p <- ggplot(data = gapminder,
             mapping = aes(x = gdpPercap, y = lifeExp))
 p + geom_point(mapping = aes(color = continent)) + geom_smooth() + scale_x_log10()
 # Try it: Notice how the above code leads to a single smooth line, not one per continent. Why?
 # Because different continents' data are in the same dataframe, and we do not separate them when making the smooth line.
-
-
 
 p <- ggplot(data = gapminder,
             mapping = aes(x = gdpPercap, y = lifeExp))
@@ -105,14 +102,41 @@ p + geom_point(mapping = aes(color = continent)) +
   geom_smooth(mapping = aes(color = continent), method = "gam")
 # Try it: What is bad about the following example, assuming the graph is the one we want?
 # The above codes make regression line for each continent, and this makes the picture look very confusing. So we should set aesthetics at the top level rather than at the individual geometry level.
-# Adjust. Setting aesthetics at the individual geometry level instead of top level make the code very confusing, and you have to set aesthetics at every single line which increase a lot of works.
+# Setting aesthetics at the individual geometry level instead of top level make the code very confusing, and you have to set aesthetics at every single line which increase a lot of works.
+
+
+
 # Ecercise 2
 
-bank_df <- read.csv("bank.csv")
+#load "bank.csv"
+bank <- read.csv("bank.csv")
 
+#First graph
+#y and loan
+n1=0  #two answers both are "yes"
+n2=0  #two answers both are "no"
+for (i in 1:length(bank$y)){
+  if (bank$y[i]==bank$loan[i]){
+    if (bank$y[i]=='yes'){n1=n1+1}
+    else{n2=n2+1}
+  }
+}
 
+#The number of people whose two answers are different
+nn=length(bank$y)-n1-n2
 
+kind=c('Either term-deposit or loan','Both term-deposit and loan','Neither term-deposit nor loan')
+number=c(nn,n1,n2)
+df=data.frame(kind=kind,number=number)
+ggplot(data=df,mapping=aes(x=kind,y=number))+
+  geom_bar(stat='identity',width=0.5,fill=c('slateblue','darkviolet','tomato'))+
+  geom_text(aes(label=number),vjust=1.5)+
+  labs(x='',y='Number of people',title='Number of people have term-deposit or loan')+
+  theme(plot.title=element_text(hjust=0.5))
 
-
+#Second graph
+ggplot(data = bank,
+       mapping = aes(x = duration, y = balance, color = education)) + geom_point() +
+       labs(x='Duration',y='Current account balance',title='Scatter plot of duration and account balance')
 
 
